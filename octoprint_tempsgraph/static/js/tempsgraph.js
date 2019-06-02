63f18f4509f8f37daaaf10a037cafde9699e5614
Drew Oprea <drewopera@gmail.com>
2019-06-02 14:58:50 +0000

    Hardcode Height to 720px and Width to 1280

--- octoprint_tempsgraph/static/js/tempsgraph.js
+++ octoprint_tempsgraph/static/js/tempsgraph.js
@@ -260,7 +260,7 @@ $(function() {
                     pad: 4
                   },
                   //width: 588,
-                  width 1310, // 1280 + 30, same as my custom .span8 width
+                  width 1280, // Same as webcam stream
                   height: 720,  // was 400, drewoprea
                   showlegend: false,
                   hovermode: "x",
